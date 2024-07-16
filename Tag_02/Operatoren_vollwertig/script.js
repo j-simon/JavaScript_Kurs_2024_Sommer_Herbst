04484d372be8fd8f358b5ba16ed2b8f2de2ef234
Jens Simon <64278468+jens-simon@users.noreply.github.com>
2024-07-16 12:13:29 +0200

    Tag 2 - nur ein Beispiel

---
+++ Tag_02/Operatoren_vollwertig/script.js
@@ -0,0 +1,82 @@
+'use strict'
+/* 
+Operand Operator Operand
+------------------------
+       Ausdruck
+*/
+console.log( 3 + 5); // => 8
+
+/* 
+   Funktion  log() 
+*/
+
+
+console.log(3%4) // => 3
+console.log(7%4) // => 3
+
+/* 
+Operand Operator Operand
+------------------------
+        Ausdruck
+*/
+// Zahlen hellgrün
+3 + 5
+3 % 4
+7 % 4
+
+// Text engl. String / (Faden von Buchstaben)
+// Text wird orange kennzeichnet
+'Jens'.length   
+
+'J' 
+'e'
+'n'
+'s'
+
+// Literale literrally
+// Datentyp text-zahl
+// string
+// number
+
+console.log(typeof 3 + 5)
+//console.log(typeof "Jens")
+
+
+console.log("Jens")
+console.log("Jans")
+
+// Pascal-Case/lower CamelCase
+// Substantive / Verben sind verboten
+// Deklaration einer Variable
+// let Schlüsselword / keyword
+let firstName = "Jens"
+let isbn
+isbn = "123333-888"
+
+
+let lastName = "Simon"
+
+
+
+// vorname
+console.log(firstName)
+console.log(lastName)
+console.log(firstName + " " + lastName)
+
+//wort="hi"
+//var wort = "hallo" 
+
+let wort ="jens"
+wort ="simon"
+
+// screaming snake case
+const MWST = 19
+//MWST = 21
+
+let bruttoPreis = 100 * (1+ MWST/100)
+console.log(bruttoPreis)
+
+bruttoPreis = 200 * (1+ MWST/100)
+console.log(bruttoPreis)
+
+
