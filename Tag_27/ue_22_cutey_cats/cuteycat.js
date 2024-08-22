251a4c77a9c5a770f9487a8672a5650512f42f09
Jens Simon <64278468+jens-simon@users.noreply.github.com>
2024-08-22 07:54:48 +0200

    Tag 27 Besprechung Übungen

--- Tag_27/ue_22_cutey_cats/cuteycat.js
+++ Tag_27/ue_22_cutey_cats/cuteycat.js
@@ -1,24 +1,24 @@
 {
     const init = () => {
 
-        const linkeListe = $("#candidates")
+        console.log("init")
         const rechteListe = $("#cutest")
+        const linkeListe = $("#candidates")
 
-        console.log($("ul"))
+        // // cloneNode-Konzept
+        // document.querySelector("#candidates li").addEventListener('click', () => {
+        //     console.log("click")
+        //     // umbewegen im DOM
+        //     //rechteListe.appendChild(linkeListe.children[0]))
 
-        /*
-        cloneNode-Konzept
-        document.querySelector("#candidates li").addEventListener('click', () =>
-            // umbewegen im DOM
-            //rechteListe.appendChild(linkeListe.children[0]))
-
-            // clone/copy
-            rechteListe.appendChild(linkeListe.children[0].cloneNode(true))
-        )
-        */
+        //     // umbewegen //clone/copy
+        //     rechteListe.appendChild(linkeListe.children[0].cloneNode(true))
+        // }
+        // )
 
 
-        // li in der linken liste wurde geclickt
+
+        // // li in der linken liste wurde geclickt
         const linkeListeKlickEventHandler= (e) => {
             console.log("click links")
 
@@ -26,13 +26,26 @@
             // maximal 3 li in der rechten Liste
             const anzahlKatzenRechts = rechteListe.querySelectorAll('li').length
             console.log('anzahlKatzenRechts --->', anzahlKatzenRechts);
-            if (anzahlKatzenRechts==3) return
+            if (anzahlKatzenRechts === 3) return
 
-            // checken, ob Katze schon in rechter Liste ist
-            const gefunden = Array.from(rechteListe.querySelectorAll('li')).findIndex(e.currentTarget)
+            //  checken, ob Katze schon in rechter Liste ist
+            console.log("e.currentTarget",e.currentTarget)
+            const gefunden = Array.from(rechteListe.querySelectorAll('li'))
+                            .findIndex((li) => {
+                                // console.log(e.currentTarget.querySelector('span').textContent)
+                                // console.log( li.querySelector('span').textContent)
+                                console.log(e.currentTarget.children[0].alt)
+                                console.log( li.children[0].alt)
+                            
+                                // Vergleich der spna-Inhalte
+                                //   return  e.currentTarget.querySelector('span').textContent === li.querySelector('span').textContent 
+                                // oder Vergleich der alt-Attribute
+                                return  e.currentTarget.children[0].alt === li.children[0].alt 
+                            })
             console.log('gefunden --->', gefunden);
-            
-            // geclicktes li Element kopieren            
+            if (gefunden != -1) return // gefunden != -1, die Katze ist schon eingefügt worden
+
+            // geklicktes li Element kopieren            
             let li=e.currentTarget.cloneNode(true)
 
             // auf dieses li click eventhandler fuer remove() implementieren)
@@ -41,10 +54,11 @@
             // li in rechte Liste appenden!
             rechteListe.appendChild(li)
             }
+
         // click eventhandler auf alle li der linken liste implementieren    
         $on($$('#candidates li'), "click",linkeListeKlickEventHandler)
-        
-        
+
+
 
 
     }
